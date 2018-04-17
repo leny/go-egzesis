64e2ab257cffda0f0990ab0cad57a9dc6081eadc
leny <info@flatland.be>
2018-04-17 15:59:52 +0200

    Base solution for exo-six

---
+++ exo-six/main.go
@@ -0,0 +1,38 @@
+// Exercise 6: Retirement Calculator
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+	var rawAge string
+	fmt.Print("What is your current age? ")
+	fmt.Scanln(&rawAge)
+	age, err := strconv.ParseInt(rawAge, 10, 16)
+	if err != nil {
+		fmt.Printf("error with age: %s\n", err)
+	}
+
+	var rawPension string
+	fmt.Print("At what age would you like to retire? ")
+	fmt.Scanln(&rawPension)
+	pension, err := strconv.ParseInt(rawPension, 10, 16)
+	if err != nil {
+		fmt.Printf("error with retire age: %s\n", err)
+	}
+
+	currentYear := time.Now().Year()
+	yearsToWork := pension - age
+	retireYear := currentYear + int( yearsToWork )
+
+	if yearsToWork <= 0 {
+		fmt.Print("You already can retire!\n")
+		return
+	}
+
+	fmt.Printf("You have %d years left until you can retire.\nIt's %d, so you can retire in %d\n", yearsToWork, currentYear, retireYear)
+}
